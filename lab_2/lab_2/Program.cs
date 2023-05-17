45df798bdaba904344554e92b451ed57a633a6d9
ItisReaper2 <aleshin.grigory@mail.ru>
2023-05-17 17:12:11 +0700

    menu

---
+++ lab_2/lab_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("Ввести А");
+            Console.WriteLine("Ввести В");
+            Console.WriteLine("Выполнить операцию “+”");
+            Console.WriteLine("выполнить операцию “-”");
+            Console.WriteLine("выполнить операцию “*”");
+            Console.WriteLine("выполнить операцию “/”");
+
+            int A = 0, B = 0, vibor = 0;
+        }
+    }
+}
